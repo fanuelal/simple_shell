a3d9a1b541ac580ffbf25545dd717113e67bc17d
Yusuf Ridwan <horiyorrmi72@gmail.com>
2021-08-20 07:31:12 -0700

    prompt.c

---
+++ prompt.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * prompt - checks mode and prints prompt in interactive mode
+ * @fd: file stream
+ * @buf: buffer
+**/
+
+void prompt(int fd, struct stat buf)
+{
+	fstat(fd, &buf);
+
+	if (S_ISCHR(buf.st_mode))
+		_puts(PROMPT);
+}
+
+/**
+ * _puts - prints a string without a \n
+ * @str: string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+unsigned int length;
+
+length = _strlen(str);
+
+write(STDOUT_FILENO, str, length);
+}
